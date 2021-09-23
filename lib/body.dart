e07781185123657a49e9b17da81d4b7c4b4383f2
Lebeil <liedel75@gmail.com>
2021-09-23 15:42:49 +0200

    add pop-up modal

--- lib/body.dart
+++ lib/body.dart
@@ -10,7 +10,7 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
-        onPressed: snack,
+        onPressed: alert,
         child: const Text('Appuie !'),
         style: ElevatedButton.styleFrom(
           primary: Colors.teal,
@@ -24,6 +24,7 @@ class _BodyState extends State<Body> {
     );
   }
 
+  //snackbar
   void snack() {
     SnackBar snackBar = const SnackBar(
       content: Text('Je suis une snackbar', textScaleFactor: 1.5),
@@ -31,6 +32,38 @@ class _BodyState extends State<Body> {
     );
 
     ScaffoldMessenger.of(context).showSnackBar(snackBar); //Scaffold.of (old)
+  }
+
+  //pop-up
+  Future<Null> alert() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false, //appuie externe pour sortir
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Ceci est une alerte', textScaleFactor: 1.5),
+          content: const Text("Houston on a un problème! On continue ?"),
+          contentPadding: const EdgeInsets.all(5),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                print('Abort');
+                Navigator.pop(context);
+              },
+              child: const Text("Annuler", style: TextStyle(color: Colors.red)),
+            ),
+            TextButton(
+              onPressed: () {
+                print('Proceed');
+                Navigator.pop(context);
+              },
+              child: const Text("Continuer", style: TextStyle(color: Colors.green)),
+            ),
+          ],
+        );
+      },
+
+    );
   }
 
 }
